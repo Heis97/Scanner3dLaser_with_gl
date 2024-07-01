ae9db332976149ce49835dd5ed961baf4f29eeb1
Heis97 <46742591+Heis97@users.noreply.github.com>
2024-07-01 16:00:26 +0300

    0107

--- opengl3/MainScanningForm.cs
+++ opengl3/MainScanningForm.cs
@@ -255,13 +255,13 @@ namespace opengl3
             // var ps_u = PathPlanner.unif_dist(ps.ToList(), 4.05);
             //   Console.WriteLine("sf");
 
-            var im_patt = new Mat("2v2.png");
-            var ps = new System.Drawing.PointF[3];
+           // var im_patt = new Mat("2v2.png");
+           // var ps = new System.Drawing.PointF[3];
             //var find = FindCircles.findCircles(im_patt,ref ps, new Size(6, 7));
 
-            var find = GeometryAnalyse.findCirclesIter(im_patt.Clone(), ref ps, new Size(6, 7));
-            CvInvoke.Imshow("find",find );
-            CvInvoke.WaitKey();
+            //var find = GeometryAnalyse.findCirclesIter(im_patt.Clone(), ref ps, new Size(6, 7));
+            //CvInvoke.Imshow("find",find );
+            //CvInvoke.WaitKey();
         }
 
         void test_handeye()
